bd5dbd838f8f7aa6385beb3ab8f4296cf9d1c613
Cabral <mateusccabr@gmail.com>
2024-09-01 20:42:49 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // This enables static exports, which GitHub Pages requires.
-    basePath: "/francyaraujo",  // Replace with your repository name.
-    assetPrefix: "/francyaraujo/", // Ensures static assets are correctly referenced.
-    images: {
-      unoptimized: true,  // Disables server-based image optimization.
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  output: "export",  // Enables static exports, which GitHub Pages requires.
+  basePath: "/francyaraujo",  // Replace with your repository name.
+  assetPrefix: "/francyaraujo/", // Ensures static assets are correctly referenced.
+  images: {
+    unoptimized: true,  // Disables server-based image optimization.
+  },
+  images: {
+    loader: "akamai",  // Use Akamai image loader for static exports.
+    path: "",  // Ensures image paths are relative.
+  },
+};
+
+module.exports = nextConfig;
