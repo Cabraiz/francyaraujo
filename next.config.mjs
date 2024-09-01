8fb5fb1e7a480cf1073355e1c3b2f5a55987df29
Cabral <mateusccabr@gmail.com>
2024-09-01 20:49:55 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,6 @@ const nextConfig = {
   basePath: "/francyaraujo",  // Replace with your repository name.
   assetPrefix: "/francyaraujo/", // Ensures static assets are correctly referenced.
   images: {
-    unoptimized: true,  // Disables server-based image optimization.
-  },
-  images: {
     loader: "akamai",  // Use Akamai image loader for static exports.
     path: "",  // Ensures image paths are relative.
   },
