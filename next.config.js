4b069b6434aad7e607a93bb4d01414699d9df583
Cabral <mateusccabr@gmail.com>
2024-09-01 20:30:43 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 // next.config.js
-module.exports = {
-    exportPathMap: async function (
-      defaultPathMap,
-      { dev, dir, outDir, distDir, buildId }
-    ) {
-      return {
-        '/': { page: '/' },
-      };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // This enables static exports, which GitHub Pages requires.
+    basePath: "/francyaraujo",  // Replace with your repository name.
+    assetPrefix: "/francyaraujo/", // Ensures static assets are correctly referenced.
+    images: {
+      unoptimized: true,  // Disables server-based image optimization.
     },
   };
+  
+  module.exports = nextConfig;
   
