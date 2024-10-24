08fa9f791ff016261c6440e44d1b6960f5f9b05c
Cabral <mateusccabr@gmail.com>
2024-10-24 01:57:04 -0300

    .

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,11 +2,6 @@ import { getAllPosts, getPostBySlug } from "@/lib/posts";
 import markdownToHtml from "@/lib/markdownToHtml";
 import PostContent from "@/app/_components/PostContent";
 
-interface PostPageProps {
-  post: Post;
-  content: string;
-}
-
 interface Author {
   name: string;
   picture: string;  // picture agora é sempre uma string
@@ -31,6 +26,7 @@ export async function generateStaticParams() {
 
 // Componente de página para renderizar o post baseado no slug
 export default async function Post({ params }: { params: { slug: string } }) {
+  // Buscando o post pelo slug
   const post = getPostBySlug(params.slug, ["slug", "title", "content", "coverImage", "date", "author"]);
 
   if (!post) {
